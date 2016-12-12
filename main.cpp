4b50641833002baa5ea1e18b5a9f1f990a34eadf
zimengpan <zimengpan@gmail.com>
2016-12-11 22:23:22 -0500

    Added Output error and Backpropagation

--- main.cpp
+++ main.cpp
@@ -24,11 +24,11 @@ struct Self{
 class Network{
     static vector <vector <double> > gaussian(int r, int c){
         vector <vector <double> > res(r, vector <double>(c,0));
-        
+
         random_device device_random_;
         default_random_engine generator(device_random_());
         normal_distribution<> distribution(0, 1.0);
-        
+
         for(int i = 0; i < r; i++) {
             //cout << i << endl;
             for (int j = 0; j < c; j++) {
@@ -40,22 +40,29 @@ class Network{
         }
         return res;
     };
-    
+
     void sigmoid(vector <vector <double> > & z){
         for(int i = 0; i < z.size(); i++)
             for(int j = 0; j < z.at(0).size(); j++)
                 z.at(i).at(j) = 1.0/(1.0 + exp(-z.at(i).at(j)));
     }
-    
-    
+
+    vector <vector <double> > dsigmoid(vector <vector <double> > & z){
+        unsigned long r = z.size(), c = z.at(0).size();
+        vector <vector <double> > res(r, vector <double> (c, 0));
+        for(int i = 0; i < z.size(); i++)
+            for(int j = 0; j < z.at(0).size(); j++)
+                res.at(i).at(j) = z.at(i).at(j)*(1-z.at(i).at(j));
+    }
+
 public:
     Self self;
-    
+
     void _init_(Self & self, const vector <int> & sizes) {
-        
+
         self.num_layers = sizes.size();
         self.sizes = sizes;
-        
+
         for (int i = 0; i < self.num_layers-1; i++) {
             self.biases.push_back(gaussian(self.sizes.at(i+1),1));
             //cout << self.biases.at(i).at(0).at(0) << endl;
@@ -64,41 +71,65 @@ public:
             }
         }
     };
-    
-    void feedforward(Self & self, vector <vector <double> >& a){
-        // Assume that the input a is an n-element vector
-        
-        // Some changes here!!
-        for(int i = 0; i < self.num_layers; i++) {
-            vector <vector <double> > weighted = LinearAlgebra::dot(self.weights.at(i), a);
-            cout << weighted.size() << " " << weighted.at(0).size() << endl;
-            sigmoid(weighted);
-            for(int j = 0; j < weighted.size(); j++)
-                
-                a = LinearAlgebra::sum(weighted, self.biases.at(i));
+
+    vector < vector <double> > feedforward(Self & self, vector <vector <double> >& a){
+        // Feedforward
+        // Pass results to a and Return last weighted input
+        vector < vector <double> > weighted;
+        for(int i = 0; i < self.num_layers-1; i++) {
+            // Compute z^{l} = w^{l}a^{l-1} + b^{l}
+            a = LinearAlgebra::cross(self.weights.at(i), a);
+            a = LinearAlgebra::sum(weighted, self.biases.at(i)); // Weighted input
+            // Compute a^{l} = sigma(z^{l})
+            sigmoid(a);
         }
+        a = LinearAlgebra::cross(self.weights.at(self.num_layers-1), a);
+        weighted = LinearAlgebra::sum(weighted, self.biases.at(self.num_layers-1)); // Weighted input
+        // Compute a^{l} = sigma(z^{l})
+        a = weighted;
+        sigmoid(a);
+        return weighted;
     };
+
+    vector < vector <double> > grad(vector <vector <double> >& y, vector <vector <double> >& a){
+        // Compute gradiant for cost function
+        unsigned long r = a.size(), c = a.at(0).size();
+        vector < vector <double> > gradiant (r, vector <double> (c, 0));
+        for(int i = 0; i < a.size(); i++)
+            for(int j = 0; j < a.at(0).size(); j++){
+                gradiant.at(i).at(j) = (y.at(i).at(j) - a.at(i).at(j))/(r*c);
+            }
+        return gradiant;
+    }
+
+    vector < vector <double> > error(vector <vector <double> >& y, vector <vector <double> >& a){
+        // Compute output error
+        vector < vector <double> > error (a.size(), vector <double> (a.at(0).size(), 0));
+        vector < vector <double> > gradiant = grad(y, a);
+        vector < vector <double> > derivative = dsigmoid(a);
+        return error = LinearAlgebra::hdot(gradiant, derivative);
+    }
 };
 
 
 int main() {
     Self self;
     Network network;
-    
+
     //cout << distribution(generator) << endl;
     vector <int> sizes = {2,3,1};
     network._init_(self, sizes);
-    
+
     /*vector<vector<double> > g = Network::gaussian(3, 2);*/
-     for (int i = 0; i < 2; i++) {
-     //cout << i << endl;
-         for(int j = 0; j < 1; j++) {
-             for(int k = 0; k < sizes[i+1]; k++) {
-     //cout << j << endl;
-                 //cout << self.biases.at(i).at(k).at(j) << " ";
-             }
-         }
-         //cout << endl;
+    for (int i = 0; i < 2; i++) {
+        //cout << i << endl;
+        for(int j = 0; j < 1; j++) {
+            for(int k = 0; k < sizes[i+1]; k++) {
+                //cout << j << endl;
+                //cout << self.biases.at(i).at(k).at(j) << " ";
+            }
+        }
+        //cout << endl;
     }
     vector <vector <double> >a;
     a.push_back({1});
